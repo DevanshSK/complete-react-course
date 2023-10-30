9a85acc438ef8059997d43b1ae3a2097f16ab879
DevanshSK <devanshs843@gmail.com>
2023-10-30 21:12:38 +0530

    API Handling started

---
+++ interviewDiscussion/src/App.jsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Main Value: </h1>
+      <button>Multiply</button>
+      <h2>Multiplied Value: </h2>
+    </>
+  );
+}
+
+export default App;
